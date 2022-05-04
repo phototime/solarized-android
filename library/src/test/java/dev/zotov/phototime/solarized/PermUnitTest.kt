a7554c988e292f1e5b340f308e794ba04f59440c
Yaroslav Zotov <35657233+zotovy@users.noreply.github.com>
2022-05-04 21:37:57 +0500

    test(unit): add timezone to PermUnitTest.kt

--- library/src/test/java/dev/zotov/phototime/solarized/PermUnitTest.kt
+++ library/src/test/java/dev/zotov/phototime/solarized/PermUnitTest.kt
@@ -3,6 +3,7 @@ package dev.zotov.phototime.solarized
 import org.junit.Test
 import java.time.Instant
 import java.time.ZoneOffset
+import java.util.*
 
 class PermUnitTest  {
 
@@ -13,36 +14,43 @@ class PermUnitTest  {
 
     @Test
     fun `Morning blur hour`() {
+        TimeZone.setDefault(TimeZone.getTimeZone("Asia/Yekaterinburg"))
         solarized.blueHour.morning!!.assertTime("04:06-04:46")
     }
 
     @Test
     fun sunrise() {
+        TimeZone.setDefault(TimeZone.getTimeZone("Asia/Yekaterinburg"))
         solarized.sunrise!!.assertTime("05:16")
     }
 
     @Test
     fun `Morning golden hour`() {
+        TimeZone.setDefault(TimeZone.getTimeZone("Asia/Yekaterinburg"))
         solarized.goldenHour.morning!!.assertTime("04:46-06:13")
     }
 
     @Test
     fun day() {
+        TimeZone.setDefault(TimeZone.getTimeZone("Asia/Yekaterinburg"))
         solarized.day!!.assertTime("06:13-20:10")
     }
 
     @Test
     fun `Evening golden hour`() {
+        TimeZone.setDefault(TimeZone.getTimeZone("Asia/Yekaterinburg"))
         solarized.goldenHour.evening!!.assertTime("20:10-21:37")
     }
 
     @Test
     fun sunset() {
+        TimeZone.setDefault(TimeZone.getTimeZone("Asia/Yekaterinburg"))
         solarized.sunset!!.assertTime("21:05")
     }
 
     @Test
     fun `Evening blue hour`() {
+        TimeZone.setDefault(TimeZone.getTimeZone("Asia/Yekaterinburg"))
         solarized.blueHour.evening!!.assertTime("21:37-22:18")
     }
 }
